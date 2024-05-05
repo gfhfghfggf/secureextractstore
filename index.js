1f91f0be9683e03bc91f42042e8bc85d39ad9a67
gfhfghfggf <psjcerezkelly89@hotmail.com>
2024-05-05 22:01:16 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-function minPathSum(grid) {
-  const m = grid.length;
-  const n = grid[0].length;
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  dp[0][0] = grid[0][0];
-  for (let i = 1; i < m; i++) {
-    dp[i][0] = dp[i - 1][0] + grid[i][0];
+function minCostClimbingStairs(cost) {
+  const n = cost.length;
+  const dp = new Array(n + 1).fill(0);
+  for (let i = 2; i <= n; i++) {
+    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
   }
-  for (let j = 1; j < n; j++) {
-    dp[0][j] = dp[0][j - 1] + grid[0][j];
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = grid[i][j] + Math.min(dp[i - 1][j], dp[i][j - 1]);
-    }
-  }
-  return dp[m - 1][n - 1];
+  return dp[n];
 }
