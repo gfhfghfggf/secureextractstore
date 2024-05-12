3b5807c59da88cd3482d2d9e7f240d78a732f31d
gfhfghfggf <psjcerezkelly89@hotmail.com>
2024-05-12 09:06:49 +0700

    aptos commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-// Setup the client
-const aptosConfig = new AptosConfig({ network: APTOS_NETWORK });
-const aptos = new Aptos(aptosConfig);
+function maxProfit(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+  for (const price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+  return maxProfit;
+}
